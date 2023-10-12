6bd4197019c02bba3ca4df86de14bfa3decc45b2
Alejandro <alexlopezabad13@gmail.com>
2023-10-12 18:53:21 +0200

    Preparacion Tfg

---
+++ TfgCibersugirdad/src/main/kotlin/com/alex/tfgcibersugirdad/TfgCibersugirdadApplication.kt
@@ -0,0 +1,11 @@
+package com.alex.tfgcibersugirdad
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class TfgCibersugirdadApplication
+
+fun main(args: Array<String>) {
+    runApplication<TfgCibersugirdadApplication>(*args)
+}
---
+++ TfgCibersugirdad/src/test/kotlin/com/alex/tfgcibersugirdad/TfgCibersugirdadApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.alex.tfgcibersugirdad
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class TfgCibersugirdadApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
