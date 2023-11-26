841b259d6b35b4764f51ec9f50e467d9911af392
Alejandro <alexlopezabad13@gmail.com>
2023-11-26 13:24:17 +0100

    Parte front mas arreglo back

--- TfgCiberseguridad/CiberseguradTfg/src/app/app.component.spec.ts
+++ TfgCiberseguridad/CiberseguradTfg/src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'CiberseguridadFront' title`, () => {
+  it(`should have the 'CiberseguradTfg' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('CiberseguridadFront');
+    expect(app.title).toEqual('CiberseguradTfg');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, CiberseguridadFront');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, CiberseguradTfg');
   });
 });
